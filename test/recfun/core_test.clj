1e88d32257555396d9a4aa1ab2d6caaffbb84db3
Matt Williamson <mattheww@pennwell.com>
2012-10-17 01:11:03 -0500

    cleaned up the tests some

--- test/recfun/core_test.clj
+++ test/recfun/core_test.clj
@@ -2,47 +2,30 @@
   (:use clojure.test
         recfun.core))
 
-(deftest pascal1
+(deftest pascal-test
 	(testing "col=0,row=2"
-		(is (= (pascal 0 2) 1))))
-
-(deftest pascal2
+		(is (= (pascal 0 2) 1)))
 	(testing "col=1,row=2"
-		(is (= (pascal 1 2) 2))))
-
-(deftest pascal3
+		(is (= (pascal 1 2) 2)))
 	(testing "col=1,row=3"
 		(is (= (pascal 1 3) 3))))
 
-(deftest bal1
+(deftest balance-test
 	(testing "(if (zero? x) max (/ 1 x)) is balanced?"
-		(is (balance (seq "(if (zero? x) max (/ 1 x))")))))
-
-(deftest bal2
+		(is (balance (seq "(if (zero? x) max (/ 1 x))"))))
 	(testing "'I told him ...' is balanced?"
-		(is (balance (seq "I told him (that it's not (yet) done).\n(But he wasn't listening)")))))
-
-(deftest bal3
+		(is (balance (seq "I told him (that it's not (yet) done).\n(But he wasn't listening)"))))
 	(testing "':-)' is unbalanced?"
-		(is (not (balance (seq ":-)"))))))
-
-(deftest bal4
+		(is (not (balance (seq ":-)")))))
 	(testing "Counting is not enough"
 		(is (not (balance (seq "())("))))))
-		
 
-(deftest cc1
+(deftest count-change-test
 	(testing "manual"
-		(is (= (countChange 4 (list 1 2)) 3))))
-
-(deftest cc2
+		(is (= (countChange 4 (list 1 2)) 3)))
 	(testing "sorted CHF"
-		(is (= (countChange 300 (list 5 10 20 50 100 200 500)) 1022))))
-
-(deftest cc3
+		(is (= (countChange 300 (list 5 10 20 50 100 200 500)) 1022)))
 	(testing "no pennies"
-		(is (= (countChange 301 (list 5 10 20 50 100 200 500)) 0))))
-
-(deftest cc4
+		(is (= (countChange 301 (list 5 10 20 50 100 200 500)) 0)))
 	(testing "unsorted CHF"
 		(is (= (countChange 300 (list 500 5 50 100 20 200 10)) 1022))))
